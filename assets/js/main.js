f07ddbc3214a257701de2614b7c33d3fd30e58de
Kremerin <110621622+kremerin@users.noreply.github.com>
2022-08-18 19:55:12 +0200

    Vers1

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+function change() {
+    background.style.backgroundColor = "rgba(9,143,22,1)";
+    color.style.color = "rgba(100,34,195,1)"
+};
+
+function change2() {
+    background.style.backgroundColor = "rgba(208,207,44,1)";
+    color.style.color = "rgba(50,9,143,1)"
+};
+
+function change3() {
+    background.style.backgroundColor = "rgba(240,146,26,1)";
+    color.style.color = "rgba(18,17,17,1)"
+};
+
+function change4() {
+    background.style.backgroundColor = "rgba(26,225,240,1)";
+    color.style.color = "rgba(44,61,142,1)"
+};
